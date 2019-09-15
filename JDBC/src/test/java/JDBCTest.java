7f628472f0c8dfb953e3367dc5843db5aacf456c
AnastasiiaNegro <50552968+AnastasiiaNegro@users.noreply.github.com>
2019-09-15 12:05:27 +0200

    Add files via upload

--- JDBC/src/test/java/JDBCTest.java
+++ JDBC/src/test/java/JDBCTest.java
@@ -64,10 +64,71 @@ public class JDBCTest {
                 "FROM owner;");
         assertSame(18, resultSet.getInt("age"), "Owner is ".concat(resultSet.getString("age")).concat(" years"));
     }
+
+    //Проверка на добавление данных
+    @Test
+    public void insertData() throws SQLException {
+        boolean emptyId = true;
+        Long newId = 0L;
+        while (emptyId) {
+            newId = System.currentTimeMillis();
+            ResultSet resultSet = statement.executeQuery("SELECT id FROM owner where id =" + newId);
+            emptyId = resultSet.next();
+        }
+        Long finalNewId = newId;
+        assertThrows(SQLException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                statement.execute("INSERT INTO owner (id,name,age) VALUES (" + finalNewId + ", 'Nastia', 24);");
+            }
+        });
+    }
+
+    //Добавление автомобиля с владельцем
+    @Test
+    public void insertCarWithOwner() throws SQLException {
+        boolean emptyId = true;
+        Long newId = 0L;
+        while (emptyId) {
+            newId = System.currentTimeMillis();
+            ResultSet resultSet = statement.executeQuery("SELECT id FROM car where id =" + newId);
+            emptyId = resultSet.next();
+        }
+        Long finalNewId = newId;
+        assertThrows(SQLException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                statement.execute("INSERT INTO car (id,name,id_owner) VALUES (" + finalNewId + ", 'citroen', 3);");
+            }
+        });
+    }
+
+    //Возможность изменить автомобиль у владельца
+    @Test
+    public void updateCar() throws SQLException {
+        assertThrows(SQLException.class, new Executable() {
+                    @Override
+                    public void execute() throws Throwable {
+                        statement.execute("UPDATE car SET name = 'skoda' where id=4;");
+                    }
+                }
+        );
+    }
+
+    //Возможность удаления пользователя у которого есть 3 автомобиля
+    @Test
+    public void deleteOwnerWithTwoCars() throws SQLException{
+        assertThrows(SQLException.class, new Executable() {
+                    @Override
+                    public void execute() throws Throwable {
+                        statement.execute("DELETE * FROM owner WHERE id =(SELECT owner.id FROM car\n" +
+                                "INNER JOIN owner ON car.id_owner = owner.id\n" +
+                                "GROUP by owner.name\n" +
+                                "HAVING count(owner.name) = 3);");
+                    }
+                });
+    }
 }
-
-
-
 
 //        assertSame
 //    @Test
@@ -80,13 +141,6 @@ public class JDBCTest {
 //        ResultSet resultSet = statement.executeQuery("SELECT age\n" +
 //                "FROM owner;");
 //        assertSame(years, resultSet.getInt("age"), "Owner is ".concat(resultSet.getString("age")).concat(" years"));
-
-
-
-
-
-
-
 
 
 
